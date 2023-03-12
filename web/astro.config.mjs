d9cac350e17c8058d195274af7429cca0114d07b
Louie Richardson <60019261+LouieRichardson99@users.noreply.github.com>
2023-03-12 20:48:19 +0000

    Added sitemap

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config'
 import sanity from 'astro-sanity'
 import { fileURLToPath } from 'url'
 import path, { dirname } from 'path'
+import sitemap from '@astrojs/sitemap'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 export default defineConfig({
+  site: 'https://www.louierichardson.com',
   vite: {
     resolve: {
       alias: {
@@ -20,6 +22,7 @@ export default defineConfig({
       dataset: 'production',
       apiVersion: 'v2021-10-21',
       useCdn: true
-    })
+    }),
+    sitemap()
   ]
 })
