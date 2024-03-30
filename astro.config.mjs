cd398ea308bbabbd83be3ac3987a78012bbff6e4
Louie Richardson <louie@bumpdigital.co.uk>
2024-03-30 17:33:24 +0000

    Homepage updates and formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,29 +1,29 @@
-import { defineConfig } from 'astro/config'
-import { fileURLToPath } from 'url'
-import path, { dirname } from 'path'
-import sitemap from '@astrojs/sitemap'
-import icon from 'astro-icon'
-import remarkCodeTitles from 'remark-code-titles'
+import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
+import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
+import remarkCodeTitles from "remark-code-titles";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
-  site: 'https://www.louierichardson.com',
+  site: "https://www.louierichardson.com",
   vite: {
     resolve: {
       alias: {
-        '@styles': path.resolve(__dirname, 'src/styles')
-      }
-    }
+        "@styles": path.resolve(__dirname, "src/styles"),
+      },
+    },
   },
   integrations: [sitemap(), icon()],
   markdown: {
     shikiConfig: {
-      theme: 'dark-plus',
-      langs: ['js', 'html', 'css', 'astro'],
-      wrap: false
+      theme: "dark-plus",
+      langs: ["js", "html", "css", "astro"],
+      wrap: false,
     },
-    remarkPlugins: [remarkCodeTitles]
-  }
-})
+    remarkPlugins: [remarkCodeTitles],
+  },
+});
