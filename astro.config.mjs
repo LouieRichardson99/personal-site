ee79dfcfacc1139f600a0d248c3d6d71ac6ecf4d
Louie Richardson <louie@bumpdigital.co.uk>
2024-10-26 13:03:38 +1000

    Removes unused imports in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,7 @@
 import { defineConfig } from 'astro/config';
-import { fileURLToPath } from 'url';
-import path, { dirname } from 'path';
 import sitemap from '@astrojs/sitemap';
 import icon from 'astro-icon';
 import remarkCodeTitles from 'remark-code-titles';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 export default defineConfig({
   site: 'https://www.louierichardson.com',
